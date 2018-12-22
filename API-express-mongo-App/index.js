a6e6febea8fd498375f60c1804bff958b73521ec
Krishnaraj <krishnaraj.icafs@gmail.com>
2018-12-22 13:37:33 +0530

    Organization and config

---
+++ API-express-mongo-App/index.js
@@ -0,0 +1,4 @@
+var app = require('./server/server');
+
+app.listen(4500);
+console.log('on port 4500');
--- API-express-mongo-App/server/server.js
+++ API-express-mongo-App/server/server.js
@@ -4,71 +4,24 @@ var app = express();
 var _ = require('lodash');
 var morgan = require('morgan');
 
-var lions = [];
-var id = 0;
-
-var updateId = function(req, res, next) {
-  if (!req.body.id) {
-    id++;
-    req.body.id = id + '';
-  }
-  next();
-};
+var lionRouter = require('./lions');
+var tigerRouter = require('./tigers');
 
 app.use(morgan('dev'))
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+// this is called mounting. when ever a req comes in for
+// '/lion' we want to use this router
+app.use('/lions', lionRouter);
+app.use('/tigers', tigerRouter);
 
 app.use(function(err, req, res, next) {
   if (err) {
-    res.status(500).send(error);
+    res.status(500).send(err);
   }
 });
 
-app.param('id', function(req, res, next, id) {
-  var lion = _.find(lions, {id: id});
-
-  if (lion) {
-    req.lion = lion;
-    next();
-  } else {
-    res.send();
-  }
-});
-
-app.get('/lions', function(req, res){
-  res.json(lions);
-});
-
-app.get('/lions/:id', function(req, res){
-  var lion = req.lion;
-  res.json(lion || {});
-});
-
-app.post('/lions', updateId, function(req, res) {
-  var lion = req.body;
-
-  lions.push(lion);
-
-  res.json(lion);
-});
 
 
-app.put('/lions/:id', function(req, res) {
-  var update = req.body;
-  if (update.id) {
-    delete update.id
-  }
-
-  var lion = _.findIndex(lions, {id: req.params.id});
-  if (!lions[lion]) {
-    res.send();
-  } else {
-    var updatedLion = _.assign(lions[lion], update);
-    res.json(updatedLion);
-  }
-});
-
-app.listen(4500);
-console.log('on port 4500');
+module.exports = app;
